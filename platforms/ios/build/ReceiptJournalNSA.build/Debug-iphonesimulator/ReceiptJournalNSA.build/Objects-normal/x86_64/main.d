07b3a6eb6ad7d2019ce8d9612ae3d036ca21b404
Kelvin <lie.kelvinz@gmail.com>
2018-10-29 23:18:39 +0800

    Initial Setup

---
+++ platforms/ios/build/ReceiptJournalNSA.build/Debug-iphonesimulator/ReceiptJournalNSA.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/ReceiptJournalNSA/ReceiptJournalNSA-Prefix.pch \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/main.m \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/TNSExceptionHandler.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/TNSDebugging.h \
+  /Volumes/SeagateBackup-Kelvin/NativeScript/ReceiptJournalNSA/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
